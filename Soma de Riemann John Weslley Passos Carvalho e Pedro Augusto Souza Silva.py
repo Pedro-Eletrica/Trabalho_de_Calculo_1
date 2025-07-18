8f0c67bbb70459d9b388d77c77bd618e132bf182
Pedro-Eletrica <paugusto0508@gmail.com>
2025-07-18 01:46:15 -0300

    Add files via upload

---
+++ Soma de Riemann John Weslley Passos Carvalho e Pedro Augusto Souza Silva.py
@@ -0,0 +1,14 @@
+#Soma de Riemann
+# Bibliotecas importadas para o código
+from sympy import integrate, symbols
+from sympy import tan, sec, sech, sin, cos, sinh, cosh, tanh, csc, csch, coth, cot, sqrt, sqrt_mod, euler, pi, limit, Limit, limit_seq, ln, log
+
+x = symbols('x') # Variável independente
+função = x**4 + 12*x**3 - 6*x**2 - x + 10 # Função a ser integrada
+valorxinferior = 3 # Limite inferior da integral
+valorxsuperior = 8 # Limite superior da integral
+Soma_de_Riemann = integrate(função, (x, valorxinferior, valorxsuperior)) # Integral Definida, também chamada de Integral de Riemann
+print("f(x)=", função)
+print("Limite Inferior=", valorxinferior)
+print("limite Superior=", valorxsuperior)
+print("Integral de Riemann/Integral Definida=", Soma_de_Riemann)
